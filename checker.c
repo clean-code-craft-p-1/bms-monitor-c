65458830bf07afae9a21a89ce1d927d06a956a4f
Sudeep Prasad <sudeep.rp@gmail.com>
2023-08-15 16:43:43 +0530

    vitals

--- checker.c
+++ checker.c
@@ -1,21 +1,51 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  if(temperature > 102 || temperature < 95) {
+    printf("Temperature critical!\n");
+    for (int i = 0; i < 6; i++)
+    {
+      printf("\r* ");
+      fflush(stdout);
+      sleep(1);
+      printf("\r *");
+      fflush(stdout);
+      sleep(1);
+    }
+
     return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
+  } else if(pulseRate < 60 || pulseRate > 100) {
+    printf("Pulse Rate is out of range!\n");
+    for (int i = 0; i < 6; i++)
+    {
+      printf("\r* ");
+      fflush(stdout);
+      sleep(1);
+      printf("\r *");
+      fflush(stdout);
+      sleep(1);
+    }
     return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
+  } else if(spo2 < 90) {
+    printf("Oxygen Saturation out of range!\n");
+    for (int i = 0; i < 6; i++)
+    {
+      printf("\r* ");
+      fflush(stdout);
+      sleep(1);
+      printf("\r *");
+      fflush(stdout);
+      sleep(1);
+    }
     return 0;
   }
   return 1;
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(!vitalsOk(99, 102, 70));
+  assert(vitalsOk(98.1, 70, 98));
+  printf("Done\n");
 }
